2f20a9eeb5cddfbbf1ca2ab3a09a97513bc2f89d
Bhuvan <bhuvandeshpande@gmail.com>
2023-03-26 17:41:54 +0700

    added gitignore and env file

--- neo4j/config.py
+++ neo4j/config.py
@@ -1,8 +1,12 @@
-# Update these variables based on your Neo4j configuration
-NEO4J_URI = "bolt://localhost:7687"
-NEO4J_USER = "neo4j"
-NEO4J_PASSWORD = "your_neo4j_password"
+from dotenv import load_dotenv
+import os
 
-# Set the file path and chunk size
-CSV_FILE_PATH = "your_large_file.csv"
-CHUNK_SIZE = 10000
+# Load the environment variables from the .env file
+load_dotenv()
+
+# Read the configuration values from the environment variables
+NEO4J_URI = os.environ["NEO4J_URI"]
+NEO4J_USER = os.environ["NEO4J_USER"]
+NEO4J_PASSWORD = os.environ["NEO4J_PASSWORD"]
+CSV_FILE_PATH = os.environ["CSV_FILE_PATH"]
+CHUNK_SIZE = int(os.environ["CHUNK_SIZE"])
